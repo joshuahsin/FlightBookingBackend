37bd38b8afc23dc361d5aacef9fed21dcd4e9cde
joshuahsin <jhsin1@uci.edu>
2026-01-25 20:14:44 -0800

    fix imports

--- city/views.py
+++ city/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
+from city.models import City
+from city.serializers import CitySerializer
+from user.permissions import IsAdminOrReadOnly
 
 # Create your views here.
 class CityViewSet(viewsets.ModelViewSet):
