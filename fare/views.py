8e703848679511a94523e0a7150da69a7db03078
joshuahsin <jhsin1@uci.edu>
2026-02-01 17:04:16 -0800

    Fare search by flight id and cabin class name

--- fare/views.py
+++ fare/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
 
 from fare.models import Fare
 from fare.serializers import FareSerializer
@@ -12,3 +12,25 @@ class FareViewSet(viewsets.ModelViewSet):
     queryset = Fare.objects.all()
     serializer_class = FareSerializer
     permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [SearchFilter]
+    search_fields = [
+        "cabin_class__cabin_class_name",  # search by cabin class name
+        "flight__id",                      # search by flight id
+    ]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        # Filter by flight id
+        flight_id = self.request.query_params.get("flight_id")
+        if flight_id:
+            queryset = queryset.filter(flight_id=flight_id)
+
+        # Filter by cabin class name
+        cabin_class_name = self.request.query_params.get("cabin_class")
+        if cabin_class_name:
+            queryset = queryset.filter(
+                cabin_class__cabin_class_name__iexact=cabin_class_name
+            )
+
+        return queryset
